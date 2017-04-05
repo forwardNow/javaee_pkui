ed03016087b9e89b169b7006227f37216c8622d8
吴钦飞 <wuqinfei@qq.com>
2017-04-05 18:33:15 +0800

    更新

--- WebContent/static/pkui/pkui.js
+++ WebContent/static/pkui/pkui.js
@@ -50,6 +50,11 @@ seajs.config( {
         "moment": "lib/utils/moment/2.18.0.x/moment",
         "moment-local-zh": "lib/utils/moment/2.18.0.x/locale/zh-cn",
 
+        //【lib/utils】中国身份证号校验
+        "IDValidator": "lib/utils/IDValidator/1.3.0/IDValidator",
+        // 行政区划
+        "IDValidator-GB2260": "lib/utils/IDValidator/1.3.0/GB2260",
+
         //【lib/jQuery】
         "jquery": "lib/jquery/1.11.3.x/jquery",
 
