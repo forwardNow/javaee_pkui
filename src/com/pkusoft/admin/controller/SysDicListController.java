abc274dd341e557421a15faceba10a30ac064d2c
吴钦飞 <wuqinfei@qq.com>
2017-04-08 19:25:06 +0800

    提交

--- src/com/pkusoft/admin/controller/SysDicListController.java
+++ src/com/pkusoft/admin/controller/SysDicListController.java
@@ -1,24 +1,34 @@
 package com.pkusoft.admin.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.pkusoft.admin.model.SysDept;
+import com.pkusoft.admin.model.SysDicItem;
 import com.pkusoft.admin.model.SysDicList;
-import com.pkusoft.admin.model.SysPara;
+import com.pkusoft.admin.service.SysDicItemService;
 import com.pkusoft.admin.service.SysDicListService;
 import com.pkusoft.common.cache.DicCache;
-import com.pkusoft.common.cache.SysParaCache;
 import com.pkusoft.common.constants.AdminFunctionId;
 import com.pkusoft.common.constants.AdminUrlRecource;
+import com.pkusoft.common.util.ExcelUtil;
 import com.pkusoft.common.util.LogUtils;
 import com.pkusoft.framework.controller.BaseController;
 import com.pkusoft.framework.model.Criteria;
@@ -35,11 +45,14 @@ import com.pkusoft.framework.util.WebUtils;
 @Controller
 public class SysDicListController extends BaseController {
 	/** 日志句柄 */
-	private static Logger logger = LoggerFactory.getLogger(SysDicListController.class);
+	private static Logger logger = LoggerFactory.getLogger( SysDicListController.class );
 
 	/** 服务类 */
 	@Autowired
 	private SysDicListService sysDicListService;
+	/** 服务类 */
+	@Autowired
+	private SysDicItemService sysDicItemService;
 
 	/**
 	 * 列表页面
@@ -47,12 +60,12 @@ public class SysDicListController extends BaseController {
 	 * @param model
 	 * @return
 	 */
-	@RequestMapping(AdminUrlRecource.SYS_DIC_LIST_LIST)
-	public ModelAndView sysDicListList(ModelAndView model) {
-		model.setViewName(AdminUrlRecource.SYS_DIC_LIST_LIST);
+	@RequestMapping( AdminUrlRecource.SYS_DIC_LIST_LIST )
+	public ModelAndView sysDicListList( ModelAndView model ) {
+		model.setViewName( AdminUrlRecource.SYS_DIC_LIST_LIST );
 		return model;
 	}
-	
+
 	/**
 	 * 列表数据
 	 * 
@@ -60,16 +73,16 @@ public class SysDicListController extends BaseController {
 	 * @param pager
 	 * @return
 	 */
-	@RequestMapping(value = AdminUrlRecource.SYS_DIC_LIST_LIST_DATA)
+	@RequestMapping( value = AdminUrlRecource.SYS_DIC_LIST_LIST_DATA )
 	@ResponseBody
-	public GridResult sysDicListListData(SysDicList sysDicList,Pager pager) {
+	public GridResult sysDicListListData( SysDicList sysDicList, Pager pager ) {
 		try {
-			List<SysDicList> list = sysDicListService.getSysDicListList(sysDicList, pager);
+			List<SysDicList> list = sysDicListService.getSysDicListList( sysDicList, pager );
 
-			return new GridResult(true, list, pager.getTotalRecords());
-		} catch (Exception e) {
-			logger.error("查询列表数据出错", e);
-			return new GridResult(false, null);
+			return new GridResult( true, list, pager.getTotalRecords() );
+		} catch ( Exception e ) {
+			logger.error( "查询列表数据出错", e );
+			return new GridResult( false, null );
 		}
 	}
 	/**
@@ -81,81 +94,80 @@ public class SysDicListController extends BaseController {
 	 */
 	@RequestMapping( "/admin/sysDicListListDataExt" )
 	@ResponseBody
-	public GridResult sysDicListListDataExt(String txtQuery) {
+	public GridResult sysDicListListDataExt( String txtQuery ) {
 		try {
-			Criteria<?> criteria = WebUtils.toCriteria(txtQuery);
-			List<SysDicList> list = sysDicListService.getSysDicListList(criteria);
+			Criteria<?> criteria = WebUtils.toCriteria( txtQuery );
+			List<SysDicList> list = sysDicListService.getSysDicListList( criteria );
 			int count = criteria.getPager() == null ? list.size() : criteria.getPager().getTotalRecords();
-			return new GridResult(true, list, count);
-		} catch (Exception e) {
-			logger.error("查询单位列表数据出错", e);
-			return new GridResult(false, null);
+			return new GridResult( true, list, count );
+		} catch ( Exception e ) {
+			logger.error( "查询单位列表数据出错", e );
+			return new GridResult( false, null );
 		}
 	}
-	
+
 	/**
 	 * 表单页面
 	 * 
 	 * @param model
 	 * @return
 	 */
-	@RequestMapping(AdminUrlRecource.SYS_DIC_LIST_FORM)
-	public ModelAndView sysDicListForm(java.lang.String dicName,ModelAndView model) {
+	@RequestMapping( AdminUrlRecource.SYS_DIC_LIST_FORM )
+	public ModelAndView sysDicListForm( java.lang.String dicName, ModelAndView model ) {
 		try {
-			if(dicName != null){
-				SysDicList sysDicList = sysDicListService.get(dicName);
-				model.addObject("sysDicList", sysDicList);
+			if ( dicName != null ) {
+				SysDicList sysDicList = sysDicListService.get( dicName );
+				model.addObject( "sysDicList", sysDicList );
 			}
-			
-			model.setViewName(AdminUrlRecource.SYS_DIC_LIST_FORM);
+
+			model.setViewName( AdminUrlRecource.SYS_DIC_LIST_FORM );
 			return model;
-		} catch (Exception e) {
-			logger.error("访问表单页面出错", e);
-			throw new RuntimeException(this.getMessage(e));
+		} catch ( Exception e ) {
+			logger.error( "访问表单页面出错", e );
+			throw new RuntimeException( this.getMessage( e ) );
 		}
 	}
-	
-	
+
 	/**
 	 * model
 	 * 
 	 * @param model
 	 * @return
 	 */
-	@RequestMapping("/admin/sysDicListModel")
+	@RequestMapping( "/admin/sysDicListModel" )
 	@ResponseBody
-	public JsonResult sysDicListModel(java.lang.String dicName) {
+	public JsonResult sysDicListModel( java.lang.String dicName ) {
 		JsonResult jsonResult = new JsonResult( true );
 		try {
 			Assert.hasText( dicName );
-			SysDicList sysDicList = sysDicListService.get(dicName);
+			SysDicList sysDicList = sysDicListService.get( dicName );
 			jsonResult.setData( sysDicList );
-		} catch (Exception e) {
-			jsonResult.setSuccess( false ); 
+		} catch ( Exception e ) {
+			jsonResult.setSuccess( false );
 			jsonResult.setMessage( "获取模型失败" );
 		}
 		return jsonResult;
 	}
-	
+
 	/**
 	 * 更新字典页面
 	 * 
 	 * @param model
 	 * @return
 	 */
-	@RequestMapping(AdminUrlRecource.SYS_DIC_LIST_UPDATE_FORM)
-	public ModelAndView sysDicListUpdateForm(java.lang.String dicName,ModelAndView model) {
+	@RequestMapping( AdminUrlRecource.SYS_DIC_LIST_UPDATE_FORM )
+	public ModelAndView sysDicListUpdateForm( java.lang.String dicName, ModelAndView model ) {
 		try {
-			if(dicName != null){
-				SysDicList sysDicList = sysDicListService.get(dicName);
-				model.addObject("sysDicList", sysDicList);
+			if ( dicName != null ) {
+				SysDicList sysDicList = sysDicListService.get( dicName );
+				model.addObject( "sysDicList", sysDicList );
 			}
-			
-			model.setViewName(AdminUrlRecource.SYS_DIC_LIST_UPDATE_FORM);
+
+			model.setViewName( AdminUrlRecource.SYS_DIC_LIST_UPDATE_FORM );
 			return model;
-		} catch (Exception e) {
-			logger.error("访问表单页面出错", e);
-			throw new RuntimeException(this.getMessage(e));
+		} catch ( Exception e ) {
+			logger.error( "访问表单页面出错", e );
+			throw new RuntimeException( this.getMessage( e ) );
 		}
 	}
 
@@ -165,92 +177,92 @@ public class SysDicListController extends BaseController {
 	 * @param sysDicList
 	 * @return
 	 */
-	@RequestMapping(AdminUrlRecource.SYS_DIC_LIST_SAVE)
+	@RequestMapping( AdminUrlRecource.SYS_DIC_LIST_SAVE )
 	@ResponseBody
-	public JsonResult sysDicListSave(SysDicList sysDicList) {
+	public JsonResult sysDicListSave( SysDicList sysDicList ) {
 		try {
-			sysDicListService.updateSysDicList(sysDicList);
-			
-			//载入缓存
-			DicCache.refresh(sysDicList.getDicName());
-			LogUtils.log(AdminFunctionId.SYS_DIC_LIST_UPDATE, sysDicList.getDicName() + "修改成功");
-			return new JsonResult(true);
-		} catch (Exception e) {
-			logger.error("保存信息出错", e);
-			return new JsonResult(false, this.getMessage(e));
+			sysDicListService.updateSysDicList( sysDicList );
+
+			// 载入缓存
+			DicCache.refresh( sysDicList.getDicName() );
+			LogUtils.log( AdminFunctionId.SYS_DIC_LIST_UPDATE, sysDicList.getDicName() + "修改成功" );
+			return new JsonResult( true );
+		} catch ( Exception e ) {
+			logger.error( "保存信息出错", e );
+			return new JsonResult( false, this.getMessage( e ) );
 		}
 	}
-	
+
 	/**
 	 * 新增
 	 * 
 	 * @param sysDicList
 	 * @return
 	 */
-	@RequestMapping(AdminUrlRecource.SYS_DIC_LIST_ADD)
+	@RequestMapping( AdminUrlRecource.SYS_DIC_LIST_ADD )
 	@ResponseBody
-	public JsonResult sysDicListAdd(SysDicList sysDicList) {
+	public JsonResult sysDicListAdd( SysDicList sysDicList ) {
 		try {
-			sysDicListService.insertSysDicList(sysDicList);
-			
+			sysDicListService.insertSysDicList( sysDicList );
+
 			// 载入缓存
-			DicCache.set(sysDicList.getDicName(), null);
-			LogUtils.log(AdminFunctionId.SYS_DIC_LIST_ADD, sysDicList.getDicName() + "新增成功");
-			return new JsonResult(true);
-		} catch (Exception e) {
-			logger.error("保存信息出错", e);
-			return new JsonResult(false, this.getMessage(e));
+			DicCache.set( sysDicList.getDicName(), null );
+			LogUtils.log( AdminFunctionId.SYS_DIC_LIST_ADD, sysDicList.getDicName() + "新增成功" );
+			return new JsonResult( true );
+		} catch ( Exception e ) {
+			logger.error( "保存信息出错", e );
+			return new JsonResult( false, this.getMessage( e ) );
 		}
 	}
-	
+
 	/**
 	 * 删除信息
 	 * 
 	 * @param dicName
 	 * @return
 	 */
-	@RequestMapping(AdminUrlRecource.SYS_DIC_LIST_DELETE)
+	@RequestMapping( AdminUrlRecource.SYS_DIC_LIST_DELETE )
 	@ResponseBody
-	public JsonResult sysDicListDelete(java.lang.String[] dicName) {
+	public JsonResult sysDicListDelete( java.lang.String[] dicName ) {
 		try {
-			sysDicListService.deleteSysDicList(dicName);
-			if(dicName.length > 0){
-				for (int i = 0; i < dicName.length; i++) {
-					DicCache.remove(dicName[i]);
-					LogUtils.log(AdminFunctionId.SYS_DIC_LIST_DELETE, dicName[i] + "删除成功");
+			sysDicListService.deleteSysDicList( dicName );
+			if ( dicName.length > 0 ) {
+				for ( int i = 0; i < dicName.length; i++ ) {
+					DicCache.remove( dicName[ i ] );
+					LogUtils.log( AdminFunctionId.SYS_DIC_LIST_DELETE, dicName[ i ] + "删除成功" );
 				}
 			}
-			return new JsonResult(true);
-		} catch (Exception e) {
-			logger.error("删除信息出错", e);
-			return new JsonResult(false, this.getMessage(e));
+			return new JsonResult( true );
+		} catch ( Exception e ) {
+			logger.error( "删除信息出错", e );
+			return new JsonResult( false, this.getMessage( e ) );
 		}
 	}
-	
+
 	/**
 	 * 生成字典文件
 	 * 
 	 * @param model
 	 * @return
 	 */
-	@RequestMapping(AdminUrlRecource.SYS_DIC_CREATE_XML_FORM)
+	@RequestMapping( AdminUrlRecource.SYS_DIC_CREATE_XML_FORM )
 	@ResponseBody
-	public JsonResult sysDicCreateXmlForm(SysDicList sysDicList) {
+	public JsonResult sysDicCreateXmlForm( SysDicList sysDicList ) {
 		try {
-			//单个生成字典文件
-			if(sysDicList.getDicName() != null && !"".equals(sysDicList.getDicName())){
-				sysDicListService.createDicFile(sysDicList.getDicName());
-				LogUtils.log(AdminFunctionId.SYS_DIC_LIST_XML_FILE, sysDicList.getDicName() + "生成字典文件成功");
-			}else{
-				//生成全部字典文件
+			// 单个生成字典文件
+			if ( sysDicList.getDicName() != null && !"".equals( sysDicList.getDicName() ) ) {
+				sysDicListService.createDicFile( sysDicList.getDicName() );
+				LogUtils.log( AdminFunctionId.SYS_DIC_LIST_XML_FILE, sysDicList.getDicName() + "生成字典文件成功" );
+			} else {
+				// 生成全部字典文件
 				sysDicListService.createAllDicFile();
-				LogUtils.log(AdminFunctionId.SYS_DIC_LIST_XML_FILE, "生成字典文件成功");
+				LogUtils.log( AdminFunctionId.SYS_DIC_LIST_XML_FILE, "生成字典文件成功" );
 			}
-			
-			return new JsonResult(true);
-		} catch (Exception e) {
-			logger.error("生成字典失败", e);
-			throw new RuntimeException(this.getMessage(e));
+
+			return new JsonResult( true );
+		} catch ( Exception e ) {
+			logger.error( "生成字典失败", e );
+			throw new RuntimeException( this.getMessage( e ) );
 		}
 	}
 	/**
@@ -259,74 +271,101 @@ public class SysDicListController extends BaseController {
 	 * @param model
 	 * @return
 	 */
-	@RequestMapping("/admin/sysDicCreateXml")
+	@RequestMapping( "/admin/sysDicCreateXml" )
 	@ResponseBody
-	public JsonResult sysDicCreateXml(String[] dicName) {
+	public JsonResult sysDicCreateXml( String[] dicName ) {
 		try {
-			//生成指定字典文件
-			if( dicName != null && dicName.length > 0){
+			// 生成指定字典文件
+			if ( dicName != null && dicName.length > 0 ) {
 				sysDicListService.createDicFile( dicName );
-				LogUtils.log(AdminFunctionId.SYS_DIC_LIST_XML_FILE, dicName + "生成指定字典文件");
-			}else{
-				//生成全部字典文件
+				LogUtils.log( AdminFunctionId.SYS_DIC_LIST_XML_FILE, dicName + "生成指定字典文件" );
+			} else {
+				// 生成全部字典文件
 				sysDicListService.createAllDicFile();
-				LogUtils.log(AdminFunctionId.SYS_DIC_LIST_XML_FILE, "生成字典文件成功");
+				LogUtils.log( AdminFunctionId.SYS_DIC_LIST_XML_FILE, "生成字典文件成功" );
 			}
-			
-			return new JsonResult(true);
-		} catch (Exception e) {
-			logger.error("生成字典失败", e);
-			throw new RuntimeException(this.getMessage(e));
+
+			return new JsonResult( true );
+		} catch ( Exception e ) {
+			logger.error( "生成字典失败", e );
+			throw new RuntimeException( this.getMessage( e ) );
 		}
 	}
-	
+
 	/**
 	 * 检查字典名称
 	 * 
 	 * @param SysDicList
-	 * @return 
+	 * @return
 	 */
-	@RequestMapping(AdminUrlRecource.CHECK_SYS_DIC_LIST_FORM)
+	@RequestMapping( AdminUrlRecource.CHECK_SYS_DIC_LIST_FORM )
 	@ResponseBody
-	public JsonResult checkSysDicListForm(java.lang.String dicName) {
+	public JsonResult checkSysDicListForm( java.lang.String dicName ) {
 		try {
-			return new JsonResult(sysDicListService.checkSysDicListForm(dicName));
-		} catch (Exception e) {
-			logger.error("检查字典名称失败", e);
-			throw new RuntimeException(this.getMessage(e));
+			return new JsonResult( sysDicListService.checkSysDicListForm( dicName ) );
+		} catch ( Exception e ) {
+			logger.error( "检查字典名称失败", e );
+			throw new RuntimeException( this.getMessage( e ) );
 		}
 	}
-	
+
 	/**
 	 * 字典名称唯一性检测
 	 * 
 	 * @param SysDicList
-	 * @return 
+	 * @return
 	 */
-	@RequestMapping("/admin/checkDicNameUnique")
+	@RequestMapping( "/admin/checkDicNameUnique" )
 	@ResponseBody
-	public JsonResult checkDicNameUnique(String dicName, String oldDicName) {
+	public JsonResult checkDicNameUnique( String dicName, String oldDicName ) {
 		try {
 			boolean isUnique = true;
 			// 新增
 			if ( oldDicName == null ) {
-				isUnique = ! sysDicListService.checkSysDicListForm(dicName);
+				isUnique = !sysDicListService.checkSysDicListForm( dicName );
 			}
 			// 修改
 			else {
 				if ( oldDicName.equals( dicName ) ) {
 					isUnique = true;
-				}
-				else {
-					isUnique = ! sysDicListService.checkSysDicListForm(dicName);
+				} else {
+					isUnique = !sysDicListService.checkSysDicListForm( dicName );
 				}
 			}
-			
-			return new JsonResult(isUnique, "字典名称唯一");
-		} catch (Exception e) {
-			logger.error("检查字典名称失败", e);
-			throw new RuntimeException(this.getMessage(e));
+
+			return new JsonResult( isUnique, "字典名称唯一" );
+		} catch ( Exception e ) {
+			logger.error( "检查字典名称失败", e );
+			throw new RuntimeException( this.getMessage( e ) );
 		}
 	}
 
+	/**
+	 * 文件下载
+	 * 
+	 * @Description:
+	 * @param fileName
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping( "/admin/sysDeptDownload" )
+	public void downloadFile( String dicName, HttpServletResponse response ) {
+		try {
+			Assert.hasText( dicName );
+			//response.setCharacterEncoding("utf-8");  
+			response.setContentType( "application/force-download" );// 设置强制下载不打开
+			response.addHeader( "Content-Disposition", "attachment;fileName=" + dicName + ".xls" );// 设置文件名
+
+			ServletOutputStream outputStream = response.getOutputStream();
+
+			List<SysDicItem> sysDicItemList = sysDicItemService.getListByProperty( "dicName", dicName );
+			String[] header = { "条目编号(itemCode)", "条目名称(itemValue)", "是否有效(visiable)" };
+			String[] propertyName = { "itemCode", "itemValue", "visiable" };
+			ExcelUtil.fillData( sysDicItemList, outputStream, header, propertyName );
+
+		} catch ( Exception e ) {
+			throw new RuntimeException( e );
+		}
+	}
 }
--- src/com/pkusoft/common/util/ExcelUtil.java
+++ src/com/pkusoft/common/util/ExcelUtil.java
@@ -1,12 +1,31 @@
 package com.pkusoft.common.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import org.apache.poi.hssf.usermodel.HSSFPalette;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
+import org.junit.Test;
+
+import com.pkusoft.admin.model.SysDicItem;
 
 /**
  * 
@@ -14,23 +33,283 @@ import org.apache.poi.ss.usermodel.Workbook;
  * 
  */
 public class ExcelUtil {
+	/**
+	 * 将 beanList 填充到 Excel（xls），请执行该类的 testFillData() 方法进行测试。
+	 * @param beanList
+	 * @param outputStream
+	 * @param header 表头，如 { "条目编号(itemCode)", "条目名称(itemValue)", "是否有效(visiable)" }
+	 * @param propertyName 要填充的bean属性名，跟表头一一对应，如 { "itemCode", "itemValue", "visiable" }
+	 * @return 是否成功
+	 */
+	public static <T> boolean fillData( List<T> beanList, OutputStream outputStream, String[] header, String[] propertyName ) {
+		HSSFWorkbook wb = null;
+		Sheet sheet = null;
+		// String[] header = { "条目编号(itemCode)", "条目名称(itemValue)", "是否有效(visiable)" };
+		// String[] propertyName = { "itemCode", "itemValue", "visiable" };
+		Field[] fields = new Field[ propertyName.length ];
 
-	public static String TEMPLATE_DIR = "/template/";
+		try {
+			Class<?> beanClass = beanList.get( 0 ).getClass();
+			for ( int i = 0; i < propertyName.length; i++ ) {
+				Field field = beanClass.getDeclaredField( propertyName[ i ] );
+				field.setAccessible( true );
+				fields[ i ] = field;
+			}
 
-	public static void fillData( List<?> beanList, String templateName, OutputStream outputStream ) throws Exception {
-		Workbook wb = new HSSFWorkbook();
-		
-		// ....
-		
-		
-	    wb.write(outputStream);
-	    wb.close();
+			wb = new HSSFWorkbook();
+			sheet = wb.createSheet( "sheet-1" );
+
+			/************** 重置预设的颜色 **************/
+			
+			// 字体颜色 BLACK => #333
+			resetHSSFColor( wb, HSSFColor.BLACK.index, ( byte ) 51, ( byte ) 51, ( byte ) 51 );
+			
+		    // 表头背景色 LIGHT_BLUE 
+			resetHSSFColor( wb, HSSFColor.LIGHT_BLUE.index, (byte) 81, (byte) 130, (byte) 187 );
+			
+			// 奇数行背景色 LIGHT_CORNFLOWER_BLUE
+			resetHSSFColor( wb, HSSFColor.LIGHT_CORNFLOWER_BLUE.index, (byte) 185, (byte) 204, (byte) 227 );
+			
+		    // 偶数行背景色 ROYAL_BLUE
+			resetHSSFColor( wb, HSSFColor.ROYAL_BLUE.index, (byte) 220, (byte) 230, (byte) 241 );
+		    
+			/************** 字体 **************/
+			
+			// 字体 - 表头单元格
+			Font headerFont = getFont( wb,  ( short ) 16, "Microsoft YaHei", HSSFColor.WHITE.index );
+
+			// 字体 - 内容单元格
+			Font contentFont = getFont( wb,  ( short ) 14, "Microsoft YaHei", HSSFColor.BLACK.index );
+
+			/************** 样式 **************/
+			
+			// 样式 - 表头单元格
+			CellStyle headerStyle = 
+					getStyle( wb, headerFont, HSSFColor.LIGHT_BLUE.index, VerticalAlignment.CENTER, HSSFColor.WHITE.index  );
+			
+			// 样式 - 内容单元格（奇数行）
+			CellStyle oddRowContentStyle = 
+					getStyle( wb, contentFont, HSSFColor.LIGHT_CORNFLOWER_BLUE.index, VerticalAlignment.CENTER, HSSFColor.WHITE.index  );
+			
+			// 样式 - 内容单元格（偶数行）
+			CellStyle evenRowContentStyle =
+					getStyle( wb, contentFont, HSSFColor.ROYAL_BLUE.index, VerticalAlignment.CENTER, HSSFColor.WHITE.index  );
+
+			/************** 表头 **************/ 
+			
+			Row headerRow = sheet.createRow( 0 );
+			headerRow.setHeightInPoints( 30 );
+			for ( int i = 0; i < header.length; i++ ) {
+				Cell headerCell = headerRow.createCell( i );
+				headerCell.setCellValue( header[ i ] );
+				headerCell.setCellStyle( headerStyle );
+			}
+
+			/************** 内容 **************/ 
+			for ( int i = 0; i < beanList.size(); i++ ) {
+				short rowNum = ( short ) ( i + 1 );
+				Row contentRow = sheet.createRow( rowNum );
+				contentRow.setHeightInPoints( 24 );
+				T bean = beanList.get( i );
+				CellStyle contentStyle = ( i % 2 == 0 ? oddRowContentStyle : evenRowContentStyle ); 
+				for ( int columnIndex = 0; columnIndex < propertyName.length; columnIndex++ ) {
+					Cell contentCell = contentRow.createCell( columnIndex );
+					contentCell.setCellStyle( contentStyle );
+					Field field = fields[ columnIndex ];
+					Class<?> type = field.getType();
+					Object value = field.get( bean );
+					if ( type == String.class ) {
+						contentCell.setCellValue( ( String ) value );
+					} else if ( type == Integer.class ) {
+						contentCell.setCellValue( ( Integer ) value );
+					} else if ( type == Date.class ) {
+						contentCell.setCellValue( ( Date ) value );
+					}
+				}
+			}
+
+			// 调整列宽 以适应单元格内容
+			sheet.autoSizeColumn( 0 );
+			sheet.autoSizeColumn( 1 );
+			sheet.autoSizeColumn( 2 );
+
+			// 冻结表头
+			sheet.createFreezePane( 0, 1, 0, 1 );
+
+			wb.write( outputStream );
+			wb.close();
+			
+			return true;
+		} catch ( IOException | NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e ) {
+			e.printStackTrace();
+			return false;
+		}
 	}
-	
-	public static <T> List<T> readData( Class<T> beanClass, String templateName, InputStream inputStream ) throws Exception {
-		List<T> list = new ArrayList<T>();
-		return list;
+	/**
+	 * 设置样式
+	 * @param wb
+	 * @param font
+	 * @param backgroundColor
+	 * @param verticalAlignment
+	 * @param borderColor
+	 * @return
+	 */
+	private static CellStyle getStyle( HSSFWorkbook wb, Font font, short backgroundColor, VerticalAlignment verticalAlignment, short borderColor ) {
+		CellStyle style = wb.createCellStyle();
+		style.setFont( font );
+		style.setFillForegroundColor( backgroundColor );
+		style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		style.setVerticalAlignment( verticalAlignment );
+		
+		style.setBorderTop( BorderStyle.THIN );
+		style.setTopBorderColor( HSSFColor.WHITE.index );
+		style.setBorderRight( BorderStyle.THIN );
+		style.setRightBorderColor( HSSFColor.WHITE.index );
+		style.setBorderBottom( BorderStyle.THIN );
+		style.setBottomBorderColor( HSSFColor.WHITE.index );
+		style.setBorderLeft( BorderStyle.THIN );
+		style.setLeftBorderColor( HSSFColor.WHITE.index );
+		return style;
+	}
+	/**
+	 * 获取字体
+	 * @param wb
+	 * @param fontPoints
+	 * @param fontFamily
+	 * @param HSSFColorndex
+	 * @return
+	 */
+	private static Font getFont( HSSFWorkbook wb, short fontPoints, String fontFamily, short HSSFColorndex ) {
+		Font font = wb.createFont();
+		font.setFontHeightInPoints( fontPoints );
+		font.setFontName( fontFamily );
+		font.setColor( HSSFColorndex );
+		return font;
+	}
+	/**
+	 * 重置预设的颜色
+	 * @param wb
+	 * @param colorIndex
+	 * @param red
+	 * @param green
+	 * @param blue
+	 */
+	private static void resetHSSFColor( HSSFWorkbook wb, short colorIndex, byte red, byte green, byte blue ) {
+		HSSFPalette palette = wb.getCustomPalette();
+	    palette.setColorAtIndex(colorIndex, red, green, blue );
+	}
+	/**
+	 * 读取 Excel（xls），将其装入指定类型的 List<T>
+	 * @param beanClass bean Class
+	 * @param inputStream
+	 * @param header 表头，如 { "条目编号(itemCode)", "条目名称(itemValue)", "是否有效(visiable)" }
+	 * @param propertyName 要填充的bean属性名，跟表头一一对应，如 { "itemCode", "itemValue", "visiable" }
+	 * @return 读取后的 List<T>
+	 * @throws RuntimeException 错误异常，通过 e.getMessage() 获取是哪行哪列出错
+	 */
+	public static <T> List<T> readData( Class<T> beanClass, InputStream inputStream, String[] header,String[] propertyName  ) {
+		List<T> beanList = new ArrayList<T>();
+		
+		HSSFWorkbook wb = null;
+		Sheet sheet = null;
+		// String[] header = { "条目编号(itemCode)", "条目名称(itemValue)", "是否有效(visiable)" };
+		// String[] propertyName = { "itemCode", "itemValue", "visiable" };
+		Field[] fields = new Field[ propertyName.length ];
+		
+		int rowNum = 1;
+		int columnNum = 0;
+
+		try {
+			for ( int i = 0; i < propertyName.length; i++ ) {
+				Field field = beanClass.getDeclaredField( propertyName[ i ] );
+				field.setAccessible( true );
+				fields[ i ] = field;
+			}
+
+			wb = new HSSFWorkbook( inputStream );
+			sheet = wb.getSheetAt( 0 );
+			
+			int firstRowNum = 1; // 跳过表头
+			int lastRowNum = sheet.getLastRowNum(); // 从 0 开始
+
+			for ( rowNum = firstRowNum; rowNum <= lastRowNum; rowNum++ ) {
+				
+				Row row = sheet.getRow( rowNum );
+				
+				T bean = beanClass.newInstance();
+				
+				if ( row == null ) {
+					// TODO 错误处理
+					continue;
+				}
+				
+				beanList.add( bean );
+				
+				int lastColumnNum = row.getLastCellNum(); // 从 1 开始
+
+				for ( columnNum = 0; columnNum < lastColumnNum; columnNum++ ) {
+					Cell cell = row.getCell( columnNum, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL );
+					Field field = fields[ columnNum ];
+					Class<?> type = field.getType();
+					Object value = null;
+					if ( cell == null ) {
+						// TODO 错误处理
+					} else {
+						if ( type == String.class ) {
+							value = cell.getStringCellValue();
+						} else if ( type == Integer.class ) {
+							value = cell.getNumericCellValue();
+						} else if ( type == Date.class ) {
+							value = cell.getDateCellValue();
+						}
+						field.set( bean, value );
+					}
+				}
+			}
+			
+			
+			wb.close();
+			
+		} catch ( Exception e ) {
+			e.printStackTrace();
+			throw new RuntimeException( "处理 第[" + ( rowNum + 1 ) + "]行 第[" + ( columnNum + 1 ) + "]列 时出错。" );
+		}
+		
+		return beanList;
 	}
 
 	
+	@Test
+	public void testReadData() throws FileNotFoundException {
+		String[] header = { "条目编号(itemCode)", "条目名称(itemValue)", "是否有效(visiable)" };
+		String[] propertyName = { "itemCode", "itemValue", "visiable" };
+		InputStream inputStream = new FileInputStream( new File( "/Users/forwardNow/Desktop/test.xls" ) );
+		List<SysDicItem> sysDicItemList = null;
+		try {
+			sysDicItemList = ExcelUtil.readData( SysDicItem.class, inputStream, header, propertyName );
+		} catch ( Exception e ) {
+			System.out.println( e.getMessage() );
+		}
+		System.out.println( sysDicItemList );
+	}
+	@Test
+	public void testFillData() throws FileNotFoundException, IOException {
+		List<SysDicItem> beanList = new ArrayList<SysDicItem>();
+
+		for ( int i = 0; i < 100; i++ ) {
+			SysDicItem sysDicItem = new SysDicItem();
+			sysDicItem.setItemCode( i + "" );
+			sysDicItem.setItemValue( "值" + i );
+			sysDicItem.setVisiable( "1" );
+			beanList.add( sysDicItem );
+		}
+
+		String[] header = { "条目编号(itemCode)", "条目名称(itemValue)", "是否有效(visiable)" };
+		String[] propertyName = { "itemCode", "itemValue", "visiable" };
+		OutputStream outputStream = new FileOutputStream( new File( "/Users/forwardNow/Desktop/test.xls" ) );
+		
+		ExcelUtil.fillData( beanList, outputStream, header, propertyName );
+		
+		outputStream.close();
+	}
 }
