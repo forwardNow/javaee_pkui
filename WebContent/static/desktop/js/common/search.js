a1561fc36f8012c9bde4996ef7275856907e5959
吴钦飞 <wuqinfei@qq.com>
2017-06-15 17:41:55 +0800

    update

---
+++ WebContent/static/desktop/js/common/search.js
@@ -0,0 +1,13 @@
+/**
+ * 应用搜索模块
+ *      1. 基于 SYS_MENU 的 MENU_NAME
+ *      2. 列出符合关键字的菜单项
+ *      3. 选中菜单项并按下回车立即启动该功能
+ */
+define( function( require ) {
+
+    var
+        $ = require( "jquery" )
+    ;
+
+} );
